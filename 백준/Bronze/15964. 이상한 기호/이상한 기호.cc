45e597f23f36030252c4eef04c4aa3f398dee711
chomyeongjin <141563326+chomyeongjin@users.noreply.github.com>
2024-03-03 16:59:13 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int cal(int a, int b) {
-	return (a + b) * (a - b);
-}
-
 int main() {
-	int a, b;
+	long long a, b;
 	cin >> a >> b;
 
-	cout << cal(a, b);
+	long long result = (a + b) * (a - b);;
+
+	cout << result;
+
 	return 0;
 }
