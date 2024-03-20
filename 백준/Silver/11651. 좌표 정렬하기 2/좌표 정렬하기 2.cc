8b2a434d0b44b7cd6e6a202fe3cb32b4f6f2d532
chomyeongjin <141563326+chomyeongjin@users.noreply.github.com>
2024-03-20 23:55:24 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 116 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second) {
+		return a.first < b.first;
+	}
+	return a.second < b.second;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<pair<int, int>> v;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+
+
+	return 0;
+}
