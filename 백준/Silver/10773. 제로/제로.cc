7e4858dda05b13072eae3f2e7e23ce8526c293c3
chomyeongjin <141563326+chomyeongjin@users.noreply.github.com>
2024-03-21 03:14:41 +0900

    [Silver IV] Title: 제로, Time: 24 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	stack<int> s;
+	int k, n;
+	int sum = 0;
+
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> n;
+		if (n == 0) {
+			s.pop();
+		}
+		else {
+			s.push(n);
+		}
+	}
+
+	int len = s.size();
+	for (int i = 0; i < len; i++) {
+		if (s.size() == 0) break;
+		sum += s.top();
+		s.pop();
+	}
+
+	cout << sum;
+
+	return 0;
+}
+
